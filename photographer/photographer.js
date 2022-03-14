b8dd3f462c1fa19bd7a31e64dd27c0b1b1ba54b0
ludovic <ludovic.deckert@gmail.com>
2022-03-15 00:55:22 +0800

    lightbox trials

--- photographer/photographer.js
+++ photographer/photographer.js
@@ -7,50 +7,123 @@ console.log(urlId.searchParams.get("id"));
 // Media list - Get media from ID
 
 let medias = data.media.filter((media)=>{return media.photographerId === Number (urlId.searchParams.get("id"))})
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+// to show the dropdown button
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+// rotate arrow when clicked on
+const arrow = document.querySelector("#arrow").addEventListener("click", () =>{
+  arrow.style.transform = "rotate(180deg)";
+})
+
+
+
+// dropdown button to sort by likes / date / title (add a const for each addeventlistener ??)
+document.querySelector("#popularity").addEventListener("click", () =>{
+  const sortMedias = medias.sort((a,b) => a.likes - b.likes);
+  displayMedia(sortMedias);
+})
+document.querySelector("#date").addEventListener("click", () =>{
+  const sortMedias = medias.sort(function (x,y){
+    let a = new Date(x.date),
+        b = new Date(y.date);
+    return a - b;
+  })
+  displayMedia(sortMedias);
+});
+document.querySelector("#title").addEventListener("click", () => {
+    const sortMedias = medias.sort(function(x, y){
+      let a = x.title.toUpperCase(),
+          b = y.title.toUpperCase();
+      return a == b ? 0 : a > b ? 1 : -1;
+    })
+    displayMedia(sortMedias);
+}); 
+
+
 
 // Create Div for medias : Images and Videos
+
 
 const lightbox = document.createElement('div');
 lightbox.id = 'lightbox'
 
-const generateLightbox = media => {
+const generateButtons = (media, medias) => {
+  const lightboxContainer = document.querySelector(".lightboxContainer");
+  const nextBtn = document.querySelector(".lightboxNext");
+  const prevBtn = document.querySelector(".lightboxPrevious");
+
+  console.log(prevBtn);
+  console.log(nextBtn);
+
+  const lastArrayMedia = medias.length - 1;
+  let lightboxInitMediaIndex = medias.findIndex(item => item.id === media.id);
+
+
+  prevBtn.addEventListener('click', () => {
+    const prev = 
+      lightboxInitMediaIndex === lastArrayMedia 
+      ? 0
+      : lightboxInitMediaIndex - 1;
+    console.log(medias[prev].image);
+    console.log(medias[prev].video);
+    const mediaToDisplay = media.image 
+    ? `<img src="/assets/images/${medias[prev].image}" class="photographerWork"/>`
+    : `<video controls autoplay class="photographerWork">
+    <source src="/assets/images/${medias[prev].video}" type="video/mp4">
+    </video>`;
+    lightboxContainer.innerHTML = mediaToDisplay
+    lightboxInitMediaIndex = prev;
+      })
+
+  nextBtn.addEventListener('keydown', e => {
+    console.log(e);
+    if(e.keycode === '39')
+      e.preventDefault();
+  const next = 
+    lightboxInitMediaIndex === lastArrayMedia 
+    ? 0 
+    : lightboxInitMediaIndex + 1;
+    console.log(medias[next].image);
+    console.log(medias[next].video);
+  const mediaToDisplay = media.image // gets the next picture but not the next "lightbox.innerHTML"
+  ? `<img src="/assets/images/${medias[next].image}" class="photographerWork"/>`     
+  : `<video controls autoplay class="photographerWork">
+  <source src="/assets/images/${medias[next].video}" type="video/mp4">
+  </video>`;
+  lightboxContainer.innerHTML =  mediaToDisplay;
+  lightboxInitMediaIndex = next;
+  })
+}
+
+/**
+ *Find a way to open both images and videos, to change
+*/
+
+const generateLightbox = (media, medias) => {
   document.body.appendChild(lightbox)
   lightbox.innerHTML = `
 <button class="lightboxClose"></button>
 <button class="lightboxNext"></button>
 <button class="lightboxPrevious"></button>
 <div class="lightboxContainer">
-<img src="/assets/images/${media.image}" class="photographerWork"/>
+   <img src="/assets/images/${media.image}" class="photographerWork"/>
+   <p class="imagesNameLightbox">${media.title}</p>
 <div>
 `;
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
+
+// close on X spot 
+const closeBtn = document.querySelector(".lightboxClose");
+closeBtn.addEventListener('click', () => {
+  return lightbox.classList.remove('active')
 })
+generateButtons(media, medias);
 } 
 
-/** 
-const nextBtn = document.getElementsByClassName('.lightboxNext');
-const lastArrayMedia = medias.length -1;
-let lightboxInitMediaIndex = medias.findIndex(item => item._id === media.id);
 
-nextBtn.addEventListener('click', () => {
-  const next = lightboxInitMediaIndex === lastArrayMedia ? 0 : lightboxInitMediaIndex + 1;
-  //const mediaToDisplay = media.image ? `<img src="/assets/images/${next.image}" class="photographerWork"/>`:
-  lightboxContainer.innerHTML =  `<img src="/assets/images/${next.image}" class="photographerWork"/>`;
-  lightboxInitMediaIndex = next;
-})
-console.log(nextBtn)*/
 
-/** 
-const prevBtn = document.querySelector('.lightboxPrevious');
-
-prevBtn.addEventListener('click', () => {
-  const prev = lightboxInitMediaIndex === lastArrayMedia ? 0 : lightboxInitMediaIndex - 1;
-  // const mediaToDisplay = media.image ? `<img src="/assets/images/${next.image}" class="photographerWork"/>`:
-  lightboxContainer.innerHTML =  `<img src="/assets/images/${prev.image}" class="photographerWork"/>`;
-  lightboxInitMediaIndex = prev;
-})*/
 
 // one function per media type
 
@@ -94,7 +167,7 @@ const displayMedia = (medias) => {
       
       element.addEventListener('click', () => {
         lightbox.classList.add('active');
-        generateLightbox(media)
+        generateLightbox(media, medias);
       })    
         mediaContainer.appendChild(element);  
       }
@@ -104,9 +177,4 @@ displayMedia(medias);
 
 
 
-
-
-
-
-
-
+  
